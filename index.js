3ce763d0384a2c32573b44048f82623e0156e50e
Sean Sollestre <sean.sollestre@gmail.com>
2023-05-21 16:41:56 -0700

    Check if two images match

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
+function cardClick(event, flippedCards) {
+    let target = event.currentTarget;
+
+
+    target.classList.toggle('flipped')
+    let checkImage = $(target).find(".pokeImg")[0]
+    flippedCards.push(target)
+    $(target).off('click');
+
+    if (flippedCards.length === 2) {
+        console.log(" 2 flipped")
+        let index = 0;
+        let firstCheck, secondCheck;
+        flippedCards.forEach(card => {
+            if (index === 0) {
+                firstCheck = $(card).find(".pokeImg")[0]
+            } else {
+                secondCheck = $(card).find(".pokeImg")[0]
+            }
+            index++
+        })
+
+        if ($(firstCheck).attr("src") == $(secondCheck).attr("src")) {
+            console.log("Match")
+        } else {
+            console.log("No match")
+        }
+        flippedCards.forEach(card => {
+            setTimeout(() => {
+                $(card).toggleClass('flipped')
+                $(card).on('click', (event) => {
+                    cardClick(event, flippedCards)
+                })
+            }, 1000)
+
+        })
+        flippedCards.length = 0
+    }
+}
+
+
 const setup = async () => {
-    console.log("Hello world")
     const randomPokemon = Math.floor(Math.random() * 100) + 1;
     let pokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
     let pokemon = pokemonResponse.data
     let cardBack = `https://velvety-mousse-38f1cf.netlify.app/back.webp`
-    console.log("Pokemon")
-    console.log(pokemon)
     // Test image: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png
     let pokemonImg = pokemon.sprites.front_default
+    let flippedCards = [];
 
-    $(".pokeCard").on("click", function () {
-        console.log("Click")
-        this.classList.toggle('flipped')
+    $(".pokeCard").on("click", (event) => {
+        cardClick(event, flippedCards)
     });
+
 
     // for (let index = 0; index < 6; index++) {
     //     $('#cardGame').append(`
