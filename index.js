e8a7712fed74726f15d5432d1e037c5c83b61e53
Sean Sollestre <sean.sollestre@gmail.com>
2023-05-21 14:09:26 -0700

    Create cards with random images and a back image

---
+++ index.js
@@ -0,0 +1,24 @@
+const setup = async () => {
+    console.log("Hello world")
+    const randomPokemon = Math.floor(Math.random() * 100) + 1;
+    let pokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+    let pokemon = pokemonResponse.data
+    let cardBack = `https://velvety-mousse-38f1cf.netlify.app/back.webp`
+    console.log("Pokemon")
+    console.log(pokemon)
+    let pokemonImg = pokemon.sprites.front_default
+
+    for (let index = 0; index < 6; index++) {
+        $('#cardGame').append(`
+        <div class="card">
+        <img src="${pokemonImg}">
+        <img src="${cardBack}">
+            Potato
+        </div>
+        `)
+
+    }
+}
+
+
+$(document).ready(setup);
